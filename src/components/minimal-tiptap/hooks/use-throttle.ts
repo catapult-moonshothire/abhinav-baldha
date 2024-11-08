b651ee10657143ec02cb61b735ac85a10b61a781
catapult-moonshothire <abhinav@moonshothire.com>
2024-11-08 23:06:37 +0530

    build fix

--- src/components/minimal-tiptap/hooks/use-throttle.ts
+++ src/components/minimal-tiptap/hooks/use-throttle.ts
@@ -1,6 +1,5 @@
 "use client";
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useRef, useCallback } from "react";
 
 export function useThrottle<T extends (...args: any[]) => void>(
